2dc41be7bba5b0258e7b579f84946e14e105ac30
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2017-02-28 01:58:45 -0500

    Add all the instructions from the Shiffman video tutorial

--- orbit.js
+++ orbit.js
@@ -1,9 +1,34 @@
-var Orbit = function(x, y, r, p) {
+var Orbit = function(x, y, r, n, p) {
     this.x = x;
     this.y = y;
     this.r = r;
-    this.parent = p;
-    this.child;
-    this.speed;
-    this.angle;
+    this.angle = 0;
+    this.n = n;
+    this.speed = radians(pow(k, n - 1)) / 10;
+    this.parent = p || null;
+    this.child = null;
+};
+
+Orbit.prototype.show = function() {
+    stroke(255);
+    strokeWeight(1);
+    noFill();
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+};
+
+Orbit.prototype.addChild = function() {
+    var newRadius = this.r / 3;
+    var newX = this.x + this.r + newRadius;
+    var newY = this.y;
+    this.child = new Orbit(newX, newY, newRadius, this.n + 1, this);
+    return this.child;
+};
+
+Orbit.prototype.update = function() {
+    if (this.parent) {
+        this.angle += this.speed;
+        var rsum = this.r + this.parent.r;
+        this.x = this.parent.x + rsum * cos(this.angle);
+        this.y = this.parent.y + rsum * sin(this.angle);
+    }
 };
--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 var r = 100;
 var x, y;
+var k = -4;
 var angle = 0;
 var looping = true;
 var path = [];
+var sun;
+var end;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -12,20 +15,40 @@ function setup() {
     stroke(255);
     x = width / 2;
     y = height / 2;
+    sun = new Orbit(width / 2, height / 2, 200, 0, null);
+    sun.addChild();
+
+    var next = sun;
+    for (var i = 0; i < 10; i++) {
+        next = next.addChild();
+    }
+    end = next;
 }
 
 function draw() {
-    background(51, 15);
-    ellipse(x, y, r * 2, r * 2);
-    // var angle = 0;
-    var r2 = r * 0.5;
-    var rsum = r + r2;
-    var x2 = x + rsum * cos(angle);
-    var y2 = y + rsum * sin(angle);
-    path.push(createVector(x2, y2));
-    ellipse(x2, y2, r2 * 2, r2 * 2);
-    angle += 0.1;
+    background(51);
 
+
+    var current = sun;
+    while (current) {
+        current.update();
+        current.show();
+        current = current.child;
+    }
+
+
+
+    // sun.show();
+    // ellipse(x, y, r * 2, r * 2);
+    // // var angle = 0;
+    // var r2 = r * 0.5;
+    // var rsum = r + r2;
+    // var x2 = x + rsum * cos(angle);
+    // var y2 = y + rsum * sin(angle);
+    path.push(createVector(end.x, end.y));
+    // ellipse(x2, y2, r2 * 2, r2 * 2);
+    // angle += 0.1;
+    stroke(255, 0, 0);
     beginShape();
     for (var i = 0; i < path.length; i++) {
         vertex(path[i].x, path[i].y);
